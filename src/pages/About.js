1d3e73736f800b327b2054f155de3acb77c35a8d
Shubham Shukla <shubhamshukla1998@gmail.com>
2020-02-02 16:19:57 +0530

    about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="comtainer">
-      <h3>About</h3>
+    <div className="container">
+      <h3 className="text-center">About</h3>
     </div>
   );
 }
