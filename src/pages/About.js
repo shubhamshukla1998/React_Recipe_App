bfd5c36d73e49b1dbfd18e24a8aa11c2989c5c83
Shubham Shukla <shubhamshukla1998@gmail.com>
2020-02-02 16:42:21 +0530

    finishing about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,27 @@ import React from "react";
 export default function About() {
   return (
     <div className="container">
-      <h3 className="text-center">About</h3>
+      <div className="text-center my-4">
+        <h2 className="mb-4">About</h2>
+        <h5>
+          This Website uses a Third Party Api{" "}
+          <a
+            href="https://spoonacular.com/food-api"
+            target="_blank"
+            rel="noopener noreferrer">
+            Spoonacular Api
+          </a>
+        </h5>
+        <h5>Any Suggestion or Query? Feel Free To Contact :</h5>
+        <h5>
+          {" "}
+          <i className="fas fa-envelope"></i> shubhamshukla1998@gmail.com
+        </h5>
+        <p>
+          Made by <span className="text-slanted">Shubham Shukla</span> with{" "}
+          <i className="fas fa-pizza-slice"></i>
+        </p>
+      </div>
     </div>
   );
 }
